975dc687eab6eed2c79ce09845221edb589bb6e7
Felipe Monroy <desarrolloit@electronika.info>
2020-10-02 21:48:35 -0500

    Proyect creation

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,8 @@
+Unsplash.configure do |config|
+  config.application_access_key = "zyT-6_NBQy5Lrl02eZDEM-gx7xtFavDzQGsMdznZsGg"
+  config.application_secret = "wM4NV1cX97y6-Eb-9gi15_FkXUcfAAX8MtZxKCFFIos"
+  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  config.utm_source = "Ideaware"
+
+
+end
