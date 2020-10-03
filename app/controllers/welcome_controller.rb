af0fb267c99e79bb255032f16f52ed6bad72095c
Felipe Monroy <desarrolloit@electronika.info>
2020-10-03 00:04:50 -0500

    test code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    @auth_url = authorization
+    @ut= params[:code]
   end
 
   def authorization
     requested_scopes = ["public", "read_user", "write_likes", "read_collections"]
     auth_url = Unsplash::Client.connection.authorization_url(requested_scopes)
+    return  auth_url
   end
 end
