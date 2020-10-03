c3cd55727f4e87d32b65c2bd20b3e693e3ca89b2
Felipe Monroy <desarrolloit@electronika.info>
2020-10-02 23:24:41 -0500

    auth test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def authorization
+    requested_scopes = ["public", "read_user", "write_likes", "read_collections"]
+    auth_url = Unsplash::Client.connection.authorization_url(requested_scopes)
+  end
 end
--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,7 +1,7 @@
 Unsplash.configure do |config|
   config.application_access_key = "zyT-6_NBQy5Lrl02eZDEM-gx7xtFavDzQGsMdznZsGg"
   config.application_secret = "wM4NV1cX97y6-Eb-9gi15_FkXUcfAAX8MtZxKCFFIos"
-  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  config.application_redirect_uri = "https://ideawaretest.herokuapp.com/"
   config.utm_source = "Ideaware"
 
 
