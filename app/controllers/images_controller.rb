dfcd02b8194c96d8464ef3d94060aad95f7d894d
Felipe Monroy <desarrolloit@electronika.info>
2020-10-04 14:53:11 -0500

    list likes, user session, give like

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,19 @@
 class ImagesController < ApplicationController
   def show
     @photo = Unsplash::Photo.find(params[:id])
+    @token = cookies[:TOKEN_SESSION]
+    @token.to_s
+    @image_id = params[:id]
   end
 
   def listLikes
+    url = URI.parse("https://api.unsplash.com/users/#{cookies[:username]}/likes")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(url.to_s, {'Authorization' => "Bearer #{cookies[:TOKEN_SESSION]}"})
+    res = http.request(req)
+    @images =JSON.load(res.body)
   end
+
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,15 +2,28 @@ class WelcomeController < ApplicationController
   def index
     @auth_url = authorization
     if params.has_key?(:code)
-      cookies[:code]  = params[:code]
+      oauth = Unsplash::Client.connection.authorize!(params[:code])
+      cookies[:TOKEN_SESSION]  = oauth.token
+      user_info
       redirect_to home_path
     end
-    @random_images = Unsplash::Photo.search("cats").shuffle.first(12)
+    words = ['Edificios','cats','art']
+    @random_images = Unsplash::Photo.search(words.sample).shuffle.first(12)
   end
 
   def authorization
     requested_scopes = ["public", "read_user", "write_likes", "read_collections"]
     auth_url = Unsplash::Client.connection.authorization_url(requested_scopes)
     return  auth_url
+  end
+
+  def user_info
+    url = URI.parse('https://api.unsplash.com/me')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(url.to_s, {'Authorization' => "Bearer #{cookies[:TOKEN_SESSION]}"})
+    res = http.request(req)
+    body =JSON.load(res.body)
+    cookies[:username]  = body["username"]
   end
 end
