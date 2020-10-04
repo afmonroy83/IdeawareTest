a18c7486b036831f41247555a7399c8b83d304bc
Felipe Monroy <desarrolloit@electronika.info>
2020-10-04 15:31:59 -0500

    give like from rails method

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -15,5 +15,15 @@ class ImagesController < ApplicationController
     @images =JSON.load(res.body)
   end
 
+  def giveLike
+    url = URI.parse("https://api.unsplash.com/photos/#{params[:image_id]}/like")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(url.to_s, {'Authorization' => "Bearer #{cookies[:TOKEN_SESSION]}"})
+    res = http.request(req)
+    binding.pry
+    redirect_to likes_path
+  end
+
 
 end
